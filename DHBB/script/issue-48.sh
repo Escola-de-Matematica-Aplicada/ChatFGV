9fb830e273dc50d4477b31daec3a41267f6ca1d7
Júlio César Chaves <julio.cesar.chaves@prof.fgv.edu.br>
2026-05-15 13:15:01 -0300

    adicionada pasta DHBB

---
+++ DHBB/script/issue-48.sh
@@ -0,0 +1,27 @@
+for f in *.text; do
+    sed -i '' \
+	-e 's/[[:<:]]assembléia[[:>:]]/assembleia/g' \
+	-e 's/[[:<:]]assembléias[[:>:]]/assembleias/g' \
+	-e 's/[[:<:]]Assembléia[[:>:]]/Assembleia/g' \
+	-e 's/[[:<:]]Assembléias[[:>:]]/Assembleias/g' \
+	-e 's/[[:<:]]idéia[[:>:]]/ideia/g' \
+	-e 's/[[:<:]]idéias[[:>:]]/ideias/g' \
+	-e 's/[[:<:]]Idéia[[:>:]]/Ideia/g' \
+	-e 's/[[:<:]]Idéias[[:>:]]/Ideias/g' \
+	-e 's/[[:<:]]conseqüente[[:>:]]/consequente/g' \
+	-e 's/[[:<:]]freqüentou[[:>:]]/frequentou/g' \
+	-e 's/[[:<:]]freqüencia[[:>:]]/frequencia/g' \
+	-e 's/[[:<:]]freqüencias[[:>:]]/frequencias/g' \
+	-e 's/[[:<:]]freqüente[[:>:]]/frequente/g' \
+	-e 's/[[:<:]]freqüentes[[:>:]]/frequentes/g' \
+	-e 's/[[:<:]]freqüentemente[[:>:]]/frequentemente/g' \
+	-e 's/[[:<:]]conseqüência[[:>:]]/consequência/g' \
+	-e 's/[[:<:]]conseqüências[[:>:]]/consequências/g' \
+	-e 's/[[:<:]]conseqüentemente[[:>:]]/consequentemente/g' \
+	-e 's/[[:<:]]co-autor[[:>:]]/coautor/g' \
+	-e 's/[[:<:]]co-autoria[[:>:]]/coautoria/g' \
+	-e 's/[[:<:]]consequencia[[:>:]]/consequência/g' \
+	-e 's/[[:<:]]consequencias[[:>:]]/consequências/g' \
+	-e 's/[[:<:]]déficit[[:>:]]/deficit/g' \
+	-e 's/[[:<:]]apóia[[:>:]]/apoia/g' $f;
+done
